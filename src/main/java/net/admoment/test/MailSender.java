4734ef30a8457712576349437592245417c002fa
Artem <artem@admoment.ru>
2013-02-25 19:22:09 +0400

    Fixed_1

--- src/main/java/net/admoment/test/MailSender.java
+++ src/main/java/net/admoment/test/MailSender.java
@@ -1,45 +1,45 @@
 package net.admoment.test;
 
+import org.apache.log4j.*;
+
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
 import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.*;
 import java.io.File;
-
 import java.util.Iterator;
 import java.util.Properties;
 import java.util.Set;
-import java.util.regex.*;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class MailSender implements MailService {
+    static final String SDOMEN= "[a-z][a-z[0-9]\u005F\u002E\u002D]*[a-z||0-9]";
+    static final Pattern P = Pattern.compile(SDOMEN + "@" + SDOMEN + "\u002E" + SDOMEN);
 
     private String host = "secure.emailsrvr.com";
 
     private String login;
     private String password;
     private String subject;
-    private Set<String> recipients;
+    private Set<Address> recipients;
     private String text;
     private String HTML;
     private Set<File> attachments;
 
+    private static final Logger log = Logger.getLogger(MailSender.class);
 
     private MailSender(){
 
     }
 
-    @Override
-    public void send() {
+
+    private Session getSession(){
         // Get system properties
         Properties properties = System.getProperties();
-
         // Setup mail server
         properties.setProperty("mail.smtp.host", host);
         properties.setProperty("mail.smtp.auth", "true");
@@ -52,21 +52,20 @@ public class MailSender implements MailService {
                                 login, password);
                     }
                 });
+        return session;
+    }
 
+    private MimeMessage getMessage(){
+        MimeMessage message = null;
         try {
             // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
+            message = new MimeMessage(this.getSession());
 
             // Set From: header field of the header.
             message.setFrom(new InternetAddress(login));
 
             // Set To: header field of the header.
-            Set<Address> array;
-            array = new InternetAddress[recipients.length];
-            for (int i = 0; i < recipients.length; i ++){
-                array[i] = new InternetAddress(recipients[i]);
-            }
-
+            Address[] array = recipients.toArray(new Address[recipients.size()]);
             message.addRecipients(Message.RecipientType.BCC, array);  //no bcc but different
 
             // Set Subject: header field
@@ -83,7 +82,6 @@ public class MailSender implements MailService {
                 messageBodyPart.setContent(this.HTML, "text/html");
             }
 
-
             // Create a multipar message
             Multipart multipart = new MimeMultipart();
             // Set text message part
@@ -91,68 +89,88 @@ public class MailSender implements MailService {
 
 
             for(Iterator<File> i = attachments.iterator(); i.hasNext(); ){
-                    File item = i.next();
-                    messageBodyPart = new MimeBodyPart();
-                    DataSource source = new FileDataSource(item);
-                    messageBodyPart.setDataHandler(new DataHandler(source));
-                    messageBodyPart.setFileName(item.getName());
-                    multipart.addBodyPart(messageBodyPart);
+                File item = i.next();
+                messageBodyPart = new MimeBodyPart();
+                DataSource source = new FileDataSource(item);
+                messageBodyPart.setDataHandler(new DataHandler(source));
+                messageBodyPart.setFileName(item.getName());
+                multipart.addBodyPart(messageBodyPart);
             }
-
 
             // Send the complete message parts
             message.setContent(multipart);
 
-            // Send message
-            Transport.send(message);
-            System.out.println("Sent message successfully....");
-        } catch (MessagingException mex) {
+        }catch (MessagingException mex) {
             mex.printStackTrace();
+            log.error(mex);
+        }
+        return message;
+    }
+
+    @Override
+    public void send() {
+         try{
+            // Send message
+            Transport.send(this.getMessage());
+            log.debug("message sent");
+        } catch (MessagingException mex) {
+            log.error(mex);
         }
     }
 
     @Override
     public void addSubject(String string) {
-        this.subject = checkNotNull(subject);
+        try{
+            this.subject = checkNotNull(subject);
+        }   catch (NullPointerException e1){
+            log.error(e1);
+        }
     }
 
     @Override
     public void addRecipient(String... recipient) {
-        String[]rec = new String[this.recipients.length + recipient.length];
-        for (int i = 0; i < this.recipients.length; i ++) {
-            rec[i] = this.recipients[i];
+        String sDomen = "[a-z][a-z[0-9]\u005F\u002E\u002D]*[a-z||0-9]";
+        String sDomen2 = "(net||org||ru||info||com)";
+        try{
+            for (int i = 0; i < checkNotNull(recipient).length; i++){
+                Matcher m =  P.matcher(recipient[i]);
+                checkArgument(m.matches());
+                this.recipients.add(new InternetAddress(recipient[i]));
+            }
+        }  catch (AddressException e) {
+            log.error(e);
+        }   catch (NullPointerException e1){
+            log.error(e1);
         }
-        for (int i = 0; i < recipients.length; i ++){
-            rec[i + this.recipients.length] = recipients[i];
-        }
-        this.recipients = rec;
     }
 
     @Override
     public void addBody(String text, boolean isHTML) {
         if (isHTML){
             if (this.HTML == null){
-                this.HTML = text;
+                this.HTML = checkNotNull(text);
             } else {
                 StringBuffer sbuf = new StringBuffer(this.HTML);
-                this.HTML = new String(sbuf.append(text));
+                this.HTML = new String(sbuf.append(checkNotNull(text)));
             }
         } else {
             if (this.text == null){
-                this.text = text;
+                this.text = checkNotNull(text);
             } else {
                 StringBuffer sbuf = new StringBuffer(this.text);
-                this.text = new String(sbuf.append(text));
+                this.text = new String(sbuf.append(checkNotNull(text)));
             }
          }
     }
 
     @Override
     public void addAttachment(File filename) {
-        this.attachments.add(checkNotNull(filename));
+        try{
+            this.attachments.add(checkNotNull(filename));
+        }catch (NullPointerException e1){
+            log.error(e1);
+        }
     }
-
-
 
 
     public String getHost() {
@@ -171,7 +189,7 @@ public class MailSender implements MailService {
         return subject;
     }
 
-    public Set<String> getRecipients() {
+    public Set<Address> getRecipients() {
         return recipients;
     }
 
@@ -192,11 +210,11 @@ public class MailSender implements MailService {
     }
 
     public static class MailBuilder implements Builder<MailSender>{
+        private String host = "secure.emailsrvr.com";
 
         private String login;
         private String password;
-        private String host;
-        private Set<String> recipient;
+        private Set<Address> recipient;
         final MailSender sender;
 
         public MailBuilder(MailSender sender){
@@ -210,26 +228,44 @@ public class MailSender implements MailService {
         }
 
         public void withLogin(String login){
-            this.login = checkNotNull(login);
+            try{
+                this.login = checkNotNull(login);
+            }   catch (NullPointerException e1){
+                log.error(e1);
+            }
         }
 
         public void withPassword(String password){
-            this.password = checkNotNull(password);
+            try{
+                this.password = checkNotNull(password);
+            }catch (NullPointerException e1){
+                log.error(e1);
+            }
         }
 
         public void withHost(String hostname) {
-            this.host = hostname;
+            try{
+                this.host = checkNotNull(hostname);
+            }   catch (NullPointerException e1){
+                log.error(e1);
+            }
         }
 
         public void withRecipient(String... recipient){
-            String sDomen = "[a-z][a-z[0-9]\u005F\u002E\u002D]*[a-z||0-9]";
-            String sDomen2 = "(net||org||ru||info||com)";
-            for (int i = 0; i < checkNotNull(recipient).length; i++){
-                checkArgument(recipient[i].matches(sDomen + "@" + sDomen + "\u002E" + sDomen2));
-                this.recipient.add(recipient[i]);
-            }
 
+            try{
+                for (int i = 0; i < checkNotNull(recipient).length; i++){
+                    Matcher m =  P.matcher(recipient[i]);
+                    checkArgument(m.matches());
+                    this.recipient.add(new InternetAddress(recipient[i]));
+                }
+            }  catch (AddressException e) {
+               log.error(e);
+            }   catch (NullPointerException e1){
+               log.error(e1);
+            }
         }
+
 
     }
 }
